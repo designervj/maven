db7711e318c2a1130343b9a1ed3e3c00b88cfee2
MANISH KUMAR GUPTA <manish.gupta.mech@gmail.com>
2026-06-18 13:47:22 +0530

    update the  latest push

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,18 @@
+import GetAllPage from "@/components/getallData/pageData/GetAllPage";
+import NavigationWrapper from "@/components/layout/NavigationWrapper";
+import ReduxProvider from "@/components/layout/ReduxProvider";
+
+export default function LocaleLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ReduxProvider>
+      <NavigationWrapper>
+        <GetAllPage />
+        {children}
+      </NavigationWrapper>
+    </ReduxProvider>
+  );
+}
