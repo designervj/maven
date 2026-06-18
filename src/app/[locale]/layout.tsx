af94cd30c4516de41f9c78c92a4841d249d549a4
MANISH KUMAR GUPTA <manish.gupta.mech@gmail.com>
2026-06-18 13:51:38 +0530

    update the changes

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import GetAllPage from "@/components/getallData/pageData/GetAllPage";
 import NavigationWrapper from "@/components/layout/NavigationWrapper";
 import ReduxProvider from "@/components/layout/ReduxProvider";
